6113b639f0bcc94877db54311d83178b9f268395
rua96 <rua96@hotmail.it>
2025-10-06 22:30:56 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const database = require("./models"); // sincronizza le nostre tabelle con il da
 
 // Definisci le origini consentite
 const allowedOrigins = [
+  "https://workout-4.onrender.com/users",
+  "https://workout-three-cyan.vercel.app",
   "https://workout-4.onrender.com",
   "workout-6zdwib1zw-andreas-projects-13e88ddf.vercel.app/:1",
   "https://workout-6zdwib1zw-andreas-projects-13e88ddf.vercel.app",
